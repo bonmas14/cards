d7610440557e0f1ae686edbb206b1376ce9d521c
bonmas14 <bonmas14@gmail.com>
2024-05-15 20:58:25 +0300

    card movement

--- game/src/main.c
+++ game/src/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include "raylib.h"
+#include "raymath.h"
 
-int main(void)
-{
+
+typedef enum {
+IDLE,
+ANIM_HOVER_IN,
+HOVERED,
+ANIM_HOVER_OUT,
+ANIM_PICKED,
+} GraphicsState;
+
+typedef struct {
+    Vector2 position;
+    Vector2 offset;
+    Texture texture;
+    GraphicsState state;
+    float scale;
+    float time;
+} GraphicsComponent;
+
+int main(void) {
     SetTraceLogLevel(LOG_WARNING);
 
     InitWindow(800, 600, "Cards");
@@ -10,24 +28,106 @@ int main(void)
     SetWindowState(FLAG_VSYNC_HINT 
         | FLAG_WINDOW_ALWAYS_RUN);
 
-    Texture tex = LoadTexture("resources/A.png");
+    GraphicsComponent gr = { 0 };
 
-    Image image = LoadImageFromTexture(tex);
+    gr.texture = LoadTexture("resources/A.png");
+    gr.offset = (Vector2){ -gr.texture.width / 2, -gr.texture.height / 2 };
+    gr.state = IDLE;
+    gr.scale = 1;
+    gr.time = 0;
 
-    SetWindowIcon(image);
 
-    while (true)
-    {
+    Camera2D cam = {.offset = (Vector2){400, 300}, .target = (Vector2){0, 0}, .zoom = 1};
+
+    SetMouseOffset(-400, -300);
+
+    while (true) {
         if (WindowShouldClose())
             return;
+
+        Rectangle rect = {
+			.x = (gr.position.x + gr.offset.x) * cam.zoom,
+			.y = (gr.position.y + gr.offset.y) * cam.zoom,
+			.width = gr.texture.width * cam.zoom,
+			.height = gr.texture.height * cam.zoom
+        };
+
+        float deltaTime = GetFrameTime();
+        float speed = 8;
+
+        switch (gr.state) {
+        case IDLE:
+            if (!CheckCollisionPointRec(GetMousePosition(), rect))
+                break;
+
+			gr.state = ANIM_HOVER_IN;
+			gr.time = 0;
+            break;
+        case ANIM_HOVER_IN:
+            gr.time += deltaTime * speed;
+
+            gr.scale = 1 + gr.time * gr.time;
+
+
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            {
+                gr.scale = 2;
+				gr.state = ANIM_PICKED;
+                break;
+            }
+
+            if (gr.time > 1)
+            {
+                gr.scale = 2;
+
+				gr.state = HOVERED;
+            }
+            break;
+        case HOVERED:
+            gr.scale = 2;
+            if (CheckCollisionPointRec(GetMousePosition(), rect))
+            {
+                if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+					gr.state = ANIM_PICKED;
+                break;
+            }
+			gr.state = ANIM_HOVER_OUT;
+			gr.time = 0;
+            break;
+        case ANIM_HOVER_OUT:
+            gr.time += deltaTime * speed;
+
+            gr.scale = 2 - 2 * gr.time + gr.time * gr.time;
+
+            if (gr.time > 1)
+            {
+                gr.scale = 1;
+
+				gr.state = IDLE;
+            }
+            break;
+        case ANIM_PICKED:
+            gr.position = Vector2Add(gr.position, Vector2Scale(GetMouseDelta(), 1 / cam.zoom));
+			if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            {
+                gr.state = HOVERED;
+            }
+            break;
+        }
 
         BeginDrawing();
         ClearBackground(BLACK);
 
-        DrawTexture(tex, 400 - tex.width / 2, 300 - tex.height / 2, WHITE);
+        //DrawRectangle(rect.x + 400, rect.y + 300, rect.width, rect.height, RAYWHITE);
 
-        char* message = "Hit 'ESC' to exit";
+        BeginMode2D(cam);
+
+		DrawTextureEx(gr.texture, Vector2Add(gr.position, Vector2Scale(gr.offset, gr.scale)), 0, gr.scale, WHITE);
+
+        EndMode2D();
+
         int fontSize = 16;
+        char* message = "Hit 'ESC' to exit";
 
         DrawText(message, 800 - MeasureText(message, fontSize), 600 - fontSize, fontSize, WHITE);
 
@@ -37,6 +137,5 @@ int main(void)
 
     CloseWindow();
 
-    UnloadTexture(tex);
-    UnloadImage(image);
+    UnloadTexture(gr.texture);
 }
